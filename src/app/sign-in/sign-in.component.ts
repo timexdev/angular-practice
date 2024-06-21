6325bf399b290360da5f28dc31093da6b57e320d
Peter Ajala <ajalapeter1@gmail.com>
2024-06-21 10:15:03 -0700

    worked on sign in design and users authentication

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
+  constructor (public route: Router) {}
+  public email = "";
+  public password = "";
+  public usersArray = [];
+  public message = false;
+   
+
+  ngOnInit (){
+    let getUsers:any = localStorage.getItem("taskUsers");
+    getUsers = JSON.parse(getUsers);
+    console.log(getUsers, "getUsers")
+    if (getUsers) {
+      this.usersArray = getUsers
+    } else {
+      this.usersArray = [];
+    }
+  }
+
+  signIn() {
+    let findUser = this.usersArray.find((each:any) => each.email == this.email && each.password == this.password);
+
+    if (findUser) {
+      this.route.navigate(["/addtask"])
+    }else{
+      this.message = true;
+    }
+    // console.log(findUser)
+  }
 
 }
